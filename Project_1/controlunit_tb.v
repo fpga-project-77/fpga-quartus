572f274f7d9e2af4657eebae6ab6e5f3775d6b81
Hasara Koralege <68479100+Has970211@users.noreply.github.com>
2021-06-25 14:50:09 +0530

    Update controlunit_tb.v
    final edition control unit

--- Project_1/controlunit_tb.v
+++ Project_1/controlunit_tb.v
@@ -14,103 +14,47 @@ module controlunit_tb ();
     end
 
     reg zFlag;
-    reg [7:0] REG_IR;
-    reg iROMREAD;
-    reg memREAD;
-    reg memWRITE;
-    reg [13:0] wEN;
-    reg selAR;
-    reg [3:0] busMUX;
-    reg [5:0] INC;
-	 reg [4:0] RST;
-    reg [2:0] compMUX;
-    reg [2:0] aluOP;
+    reg [7:0] INS;
+    wire iROMREAD;
+    wire memREAD;
+    wire memWRITE;
+    wire [13:0] wEN;
+    wire selAR;
+    wire [3:0] busMUX;
+    wire [5:0] INC;
+	wire [4:0] RST;
+    wire [2:0] compMUX;
+    wire [2:0] aluOP;
 
-    reg [7:0]NEXT_STATE;
-    reg [7:0]INS;
-    reg [7:0]MEM_READ;
-
-    controlunit dut (.Clk(Clk), .z(zFlag), .REG_IR(REG_IR), .iROMREAD(iROMREAD), .memREAD(memREAD), .memWRITE(memWRITE), .wEN(wEN), .selAR(selAR), .busMUX(busMUX), .INC(INC), .RST(RST), .compMUX(compMUX), .aluOP(aluOP)) ;
+    controlunit dut (.Clk(Clk), .z(zFlag), .INS(INS), .iROMREAD(iROMREAD), .memREAD(memREAD), .memWRITE(memWRITE), .wEN(wEN), .selAR(selAR), .busMUX(busMUX), .INC(INC), .RST(RST), .compMUX(compMUX), .aluOP(aluOP)) ;
     
 
     initial begin
-        NEXT_STATE <= `FETCH_3;
-
-        #(CLK_PERIOD*2);
-        @(posedge Clk);
-        REG_IR <= 8'b0001_0000;
-        iROMREAD <= 0;
-        memREAD <= 0;
-        memWRITE <= 0;
-        wEN <= 0;
-        selAR <= 0;
-        busMUX <= 0;
-        INC <= 0;
-        RST <= 0;
-        compMUX <= 0;
-        aluOP <= 0;
-        $display("%8b", NEXT_STATE);
-        $display("%8b", INS);
-        $display("%8b", MEM_READ);
-        
-        NEXT_STATE <= `FETCH_3;
-
-
-        #(CLK_PERIOD*2);
-        @(posedge Clk);
-        REG_IR <= 8'b0001_0000;
-        iROMREAD <= 0;
-        memREAD <= 0;
-        memWRITE <= 0;
-        wEN <= 0;
-        selAR <= 0;
-        busMUX <= 0;
-        INC <= 0;
-        RST <= 0;
-        compMUX <= 0;
-        aluOP <= 0;
-        $display("%8b", NEXT_STATE);
-        $display("%8b", INS);
-        $display("%8b", MEM_READ);
-        
-        NEXT_STATE <= `FETCH_3;
+        #(CLK_PERIOD*2)
+        INS <= 8'b00100000;
 
         #(CLK_PERIOD);
         @(posedge Clk);
-        REG_IR <= 8'b0010_0000;
-        iROMREAD <= 0;
-        memREAD <= 0;
-        memWRITE <= 0;
-        wEN <= 0;
-        selAR <= 0;
-        busMUX <= 0;
-        INC <= 0;
-        RST <= 0;
-        compMUX <= 0;
-        aluOP <= 0;
-        $display("%8b", NEXT_STATE);
-        $display("%8b", INS);
-        $display("%8b", MEM_READ);
         
-     
+        #(CLK_PERIOD*6);
+        
+        @(posedge Clk);
+        INS <= 8'b0001_0000; //JMP_M1
+        zFlag=0;
+        
 
+        #(CLK_PERIOD*7); 
+        @(posedge Clk);
+        INS <= 8'b0001_0000;  //JMP_M1
+        zFlag=1;
+
+        #(CLK_PERIOD*5)
+        
+    
         repeat(5) @(posedge Clk) begin
-            REG_IR <= $random;
-            iROMREAD <= $random;
-            memREAD <= $random;
-            memWRITE <= $random;
-            wEN <= $random;
-            selAR <= $random;
-            busMUX <= $random;
-            INC <= $random;
-            RST <= $random;
-            compMUX <= $random;
-            aluOP <= $random;
-            $display("%8b", NEXT_STATE);
-            $display("%8b", INS);
-            $display("%8b", MEM_READ);
+            INS <= $random;
+            zFlag <= $random;
             
-
         end
         
 
