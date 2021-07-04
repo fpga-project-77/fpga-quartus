d6f23b1d9a3e5e9f9a0d6922922a3a33aa38cf75
Ashan Thamara <ashanthamara98@gmail.com>
2021-07-05 03:30:07 +0530

    .py files added

--- 8_CORE/Python/dat_mem-ins_mem.py
+++ 8_CORE/Python/dat_mem-ins_mem.py
@@ -1,15 +1,15 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[33]:
 
 
 fd = open("matrix.txt", "r")
-out = open("D:\\Academic\\ACA\\SEM5 TRONIC ACA\\SEMESTER 5\\CSD\\FPGA\\00 - Git\\fpga-quartus\\Multi_CORE\\MULTI_CORE_edit\\data_mem.txt", "w")
+out = open("D:\\Academic\\ACA\\SEM5 TRONIC ACA\\SEMESTER 5\\CSD\\FPGA\\00 - Git\\fpga-quartus\\8_CORE\\MULTI_CORE_edit\\data_mem.txt", "w")
 final = open ("final.txt", "w")
 
 
-# In[2]:
+# In[34]:
 
 
 matrix1 = []
@@ -51,7 +51,7 @@ print (matrix1)
 print (matrix2)
 
 
-# In[3]:
+# In[35]:
 
 
 M = len(matrix1)
@@ -59,27 +59,29 @@ N = len(matrix1[0])
 K = len(matrix2[0])
 
 
-# In[4]:
+# In[36]:
 
 
 print (M)
+print (N)
+print (K)
 
 
-# In[5]:
+# In[37]:
 
 
 #data
-T1 = [0,0,0,0]  #rows for cores
+T1 = [0,0,0,0,0,0,0,0]  #rows for cores
 #addresses
-T7 = 4  #N
-T2 = 5  #M
-T4 = 6  #1ST MAT
-T3 = T4 + (M*N) + 4  #K
+T7 = 8  #N
+T2 = 9  #M
+T4 = 10  #1ST MAT
+T3 = T4 + (M*N) + 8  #K
 T5 = T3 + 1  #2ND MAT
 #T6 = T5 + (K*N) + 1 #3RD MAT
 
 
-# In[6]:
+# In[38]:
 
 
 print (T1)
@@ -90,7 +92,7 @@ print (T5)
 #print (T6)
 
 
-# In[7]:
+# In[39]:
 
 
 #M = 16
@@ -109,20 +111,28 @@ print ("M:", M)
 print ("No. of cores:", no_of_cores)
 
 
-# In[8]:
+# In[40]:
 
 
-T4_addr = [0,0,0,0]
-T4_addr[0] = T4 + 4
+T4_addr = [0,0,0,0,0,0,0,0]
+T4_addr[0] = T4 + 8
 if (no_of_cores > 1):
     T4_addr[1] = T4_addr[0] + T1[0]
 if (no_of_cores > 2):    
     T4_addr[2] = T4_addr[1] + T1[1]
 if (no_of_cores > 3):    
     T4_addr[3] = T4_addr[2] + T1[2] 
+if (no_of_cores > 4):    
+    T4_addr[4] = T4_addr[3] + T1[3] 
+if (no_of_cores > 5):    
+    T4_addr[5] = T4_addr[4] + T1[4] 
+if (no_of_cores > 6):    
+    T4_addr[6] = T4_addr[5] + T1[5] 
+if (no_of_cores > 7):    
+    T4_addr[7] = T4_addr[6] + T1[6] 
 
 
-# In[9]:
+# In[41]:
 
 
 d_mem_txt = ""
@@ -135,6 +145,14 @@ d_mem_txt = d_mem_txt + hex(T1[1])[2:] + "\n"
 d_mem_txt = d_mem_txt + hex(T1[2])[2:] + "\n"
 #T1_4
 d_mem_txt = d_mem_txt + hex(T1[3])[2:] + "\n"
+#T1_5
+d_mem_txt = d_mem_txt + hex(T1[4])[2:] + "\n"
+#T1_6
+d_mem_txt = d_mem_txt + hex(T1[5])[2:] + "\n"
+#T1_7
+d_mem_txt = d_mem_txt + hex(T1[6])[2:] + "\n"
+#T1_8
+d_mem_txt = d_mem_txt + hex(T1[7])[2:] + "\n"
 
 #M&N of Matrix 1
 d_mem_txt = d_mem_txt + hex(M)[2:] + "\n" + hex(N)[2:] + "\n"
@@ -147,6 +165,14 @@ d_mem_txt = d_mem_txt + hex(T4_addr[1])[2:] + "\n"
 d_mem_txt = d_mem_txt + hex(T4_addr[2])[2:] + "\n"
 #T4_4
 d_mem_txt = d_mem_txt + hex(T4_addr[3])[2:] + "\n"
+#T4_5
+d_mem_txt = d_mem_txt + hex(T4_addr[4])[2:] + "\n"
+#T4_6
+d_mem_txt = d_mem_txt + hex(T4_addr[5])[2:] + "\n"
+#T4_7
+d_mem_txt = d_mem_txt + hex(T4_addr[6])[2:] + "\n"
+#T4_8
+d_mem_txt = d_mem_txt + hex(T4_addr[7])[2:] + "\n"
 
 for i in range(N):
     for j in range(M):
@@ -160,7 +186,7 @@ for k in range(K):
 #print (d_mem_txt)
 
 
-# In[10]:
+# In[42]:
 
 
 out.write(d_mem_txt.strip())
@@ -171,14 +197,14 @@ out.close()
 
 # Instruction Memory
 
-# In[11]:
+# In[43]:
 
 
 fd = open("algo.txt", "r")
-machine_code = open("D:\\Academic\\ACA\\SEM5 TRONIC ACA\\SEMESTER 5\\CSD\\FPGA\\00 - Git\\fpga-quartus\\Multi_CORE\\MULTI_CORE_edit\\ins_mem.txt", "w")
+machine_code = open("D:\\Academic\\ACA\\SEM5 TRONIC ACA\\SEMESTER 5\\CSD\\FPGA\\00 - Git\\fpga-quartus\\8_CORE\\MULTI_CORE_edit\\ins_mem.txt", "w")
 
 
-# In[12]:
+# In[44]:
 
 
 comp_dic = {"NOOP": "00", "JPNZM": "10", "JPNZK":"11", "JPNZN":"12", "COPYRm1":"20", "COPYRk1":"21", "COPYRn1":"22",             "COPYRr1":"23", "COPYT4":"24", "LOADC1":"30", "LOADC2":"31", "STORE":"40", "ASSIGNC1":"50", "ASSIGNC2":"51", "RESETALL":"60",             "RESETRn2":"61", "RESETRk2":"62", "RESETRt":"63", "MOVERp":"70", "MOVERt":"71", "MOVEC1":"72", "MOVEC3":"73",             "SETC1":"80", "SETDR":"81", "SETRk1":"82", "MULRp":"90", "MUL_CORE":"9F", "ADDRt":"A0", "ADDRr1":"A1",             "ADDRm2":"A2", "ADD_MEM":"A3", "INCC2":"B0", "INCC3":"B1", "INCRm2":"B2", "INCRk2":"B3", "INCRn2":"B4",             "END":"C0", "CHK_IDLE":"D0", "GET_C1":"E0"}
@@ -198,7 +224,7 @@ comp_dic["T5"] = hex(T5)[2:]
 
 
 
-# In[13]:
+# In[45]:
 
 
 mchine_txt = ""
@@ -221,7 +247,7 @@ for line in fd:
         addr_flag = True
 
 
-# In[14]:
+# In[46]:
 
 
 machine_code.write(mchine_txt.strip())
@@ -231,7 +257,7 @@ machine_code.close()
 
 # Matrix Multiplication
 
-# In[15]:
+# In[47]:
 
 
 new_mat1 = []
@@ -244,7 +270,7 @@ print (new_mat1)
 print (new_mat2)
 
 
-# In[16]:
+# In[48]:
 
 
 import numpy as np
